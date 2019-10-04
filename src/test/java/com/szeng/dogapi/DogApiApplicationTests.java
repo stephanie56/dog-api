820281761f7bf96fc9be8d81be19652954f6ceaf
Stephanie Zeng <stephanie.zeng@rangle.io>
2019-10-04 17:04:20 -0400

    Initial commit: Set up the H2 in-memory database

---
+++ src/test/java/com/szeng/dogapi/DogApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.szeng.dogapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DogApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
