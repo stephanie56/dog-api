f88754a89016043fa893d438d315906398f09f81
Stephanie Zeng <stephanie.zeng@rangle.io>
2019-10-06 11:41:17 -0400

    Add sql file for the dog database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO dog (id, name, breed, origin) VALUES (1, 'Henry', 'Pug', 'Britain');
+INSERT INTO dog (id, name, breed, origin) VALUES (2, 'Tony', 'Karelian Bear', 'Finland');
+INSERT INTO dog (id, name, breed, origin) VALUES (3, 'Missy', 'Afador', 'Canada');
+INSERT INTO dog (id, name, breed, origin) VALUES (4, 'Aiajiang', 'Auggie', 'Japan');
